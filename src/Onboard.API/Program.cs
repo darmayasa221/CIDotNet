b6187b50efb02e21c445434b8904a08c16d3b0f8
Rudy Prabowo <rudy.prabowo@taksu.tech>
2023-09-01 16:58:59 +0800

    update casbin

--- src/Onboard.API/Program.cs
+++ src/Onboard.API/Program.cs
@@ -34,7 +34,7 @@ app.UseSwagger();
 // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 // middleware chasbin 
-// app.UseMiddleware<MiddlewareCasbin>();
+app.UseMiddleware<MiddlewareCasbin>();
 // end
 app.UseEndpoints(endpoints =>
 {
--- src/Onboard.Infrastructure/Middleware/Casbin.cs
+++ src/Onboard.Infrastructure/Middleware/Casbin.cs
@@ -13,6 +13,8 @@ public class MiddlewareCasbin
   private string path { get; set; }
   private string method { get; set; }
 
+  private string role { get; set; }
+
   public MiddlewareCasbin(RequestDelegate next, IRepository<AUser> repository, CancellationToken cancellationToken = new())
   {
     _repository = repository;
@@ -23,12 +25,17 @@ public class MiddlewareCasbin
   public async Task InvokeAsync(HttpContext context)
   {
     path = context.Request.Path.ToString();
+    if(path == "/swagger/index.html")
+    {
+      await _next.Invoke(context);
+      return;
+    }
     method = context.Request.Method.ToString();
     if (context.Request.Headers.TryGetValue("userId", out var userIdHeader))
     {
       var userId = Guid.Parse(userIdHeader);
-      var existingProject = await _repository.GetByIdAsync(userId, _cancellationToken);
-      if (existingProject == null)
+      var user = await _repository.GetByIdAsync(userId, _cancellationToken);
+      if (user == null)
       {
         await context.Response.WriteAsync("user not found");
         return;
@@ -36,10 +43,12 @@ public class MiddlewareCasbin
 
       var e = new Enforcer("model.conf", "policy.csv");
 
-      var flag = await e.EnforceAsync("user", path, method);
+      role = user.RoleId == 1 ? "admin" : "employee";
+
+      var flag = await e.EnforceAsync(role, path, method);
       if (!flag)
       {
-        await context.Response.WriteAsync("not user");
+        await context.Response.WriteAsync("not admin");
         return;
       }
       await _next.Invoke(context);
